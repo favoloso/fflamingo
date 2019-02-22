1ff2a7f0f028173b613ad41a6d2add0c1cc66244
Leonardo Ascione <leonardo.ascione@gmail.com>
2019-02-22 14:17:29 +0100

    Aggiunti test su CircleCI

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   projects: ['<rootDir>/packages/*'],
-  // testPathIgnorePatterns: ['**/lib/*'],
+  reporters: ['default', 'jest-junit'],
   collectCoverageFrom: [
     '<rootDir>/packages/*/src/**/*.{ts,js}?(x)',
     '!<rootDir>/packages/*/src/**/*.{spec,test}.{ts,js}?(x)'
